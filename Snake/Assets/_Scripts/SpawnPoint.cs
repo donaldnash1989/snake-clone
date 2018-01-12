4c3b922b122b0d22b3fa97f962b4d038ff872696
Donald Nash <donaldnash1989@gmail.com>
2018-01-12 12:59:26 -0500

    Finished up the basic menus and scoring

--- Snake/Assets/_Scripts/SpawnPoint.cs
+++ Snake/Assets/_Scripts/SpawnPoint.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour {
-    
+
     public static bool nextSpawn = true;
+    public GameObject probe;
     public GameObject pickup;
+
+    public static float xLimit;
+    public static float ylimit;
 
 	void Start () {
         GameManager.CreatePlayer(transform.position);
 	}
 	
-	void Update () {
+	void Update ()
+    {
         if (nextSpawn)
         {
             Vector3 spawnPoint = new Vector3(Random.Range(-20, 20), Random.Range(-11, 11), 0.0f);
             Instantiate(pickup, spawnPoint, Quaternion.identity);
             nextSpawn = false;
         }
-	}
+    }
 }
--- Snake/Assets/_Scripts/System/Helpers/ResetHelper.cs
+++ Snake/Assets/_Scripts/System/Helpers/ResetHelper.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResetHelper : MonoBehaviour {
-    
-	void Start () {
-        Player.GetInstance().Reset();
 
+    public Text yourScore;
+    public Text highScore;
+
+    void Start () {
+        if(PlayerPrefs.GetInt("HS") < Player.GetInstance().GetSegments())
+        {
+            PlayerPrefs.SetInt("HS", Player.GetInstance().GetSegments());
+        }
+        yourScore.text = "Your Score: " + Player.GetInstance().GetSegments();
+        highScore.text = "High Score: " + PlayerPrefs.GetInt("HS");
+        Player.GetInstance().Reset();
     }
 	
 	void Update () {
